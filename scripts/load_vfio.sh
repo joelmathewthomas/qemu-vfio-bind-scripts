98864f8f720d8d1f74133fee7c6e3d978ab16bfc
Joel Mathew Thomas <90510078+joelmathewthomas@users.noreply.github.com>
2024-03-21 07:37:47 +0530

    removed unnecessary info

--- scripts/load_vfio.sh
+++ scripts/load_vfio.sh
@@ -2,16 +2,18 @@
 
 cd /boot
 
+
+#deleting exisiting intiramfs to free up space on boot partition
 sudo rm initramfs-linux-fallback.img   initramfs-linux-fallback.img.tmp   initramfs-linux.img
 
 cd
 
-sudo cp /home/joel/scripts/qemu/vfio_nvidia/mkinitcpio/mkinitcpio_vfio.conf /etc/mkinitcpio.conf
+sudo cp ~/vfio_nvidia/mkinitcpio/mkinitcpio_vfio.conf /etc/mkinitcpio.conf
 echo -e "Copied mkinitcpio.conf"
 sudo mkinitcpio -P
 
 echo -e "Switching to VFIO drivers"
-sudo cp /home/joel/scripts/qemu/vfio_nvidia/grub.cfg/grub_vfio /etc/default/grub
+sudo cp ~/vfio_nvidia/grub.cfg/grub_vfio /etc/default/grub
 echo -e "Copied Grub config"
 echo "Regenerating grub"
 sudo grub-mkconfig -o /boot/grub/grub.cfg
